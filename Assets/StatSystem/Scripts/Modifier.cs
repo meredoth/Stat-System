b6e5cefc772cae1b7db240096ade40e0915db831
meredoth <52322303+meredoth@users.noreply.github.com>
2024-09-17 21:03:58 +0300

    Fixed minor bug in Default Modifier Equality

--- Assets/StatSystem/Scripts/Modifier.cs
+++ Assets/StatSystem/Scripts/Modifier.cs
@@ -7,16 +7,16 @@ namespace StatSystem
 public readonly struct Modifier : IEquatable<Modifier>
 {
    private readonly ModifierType? _type;
-   
+
    /// <summary>Gets the type of the modifier. Defaults to <see cref="ModifierType.Flat"/> if not set.</summary>
    public ModifierType Type => _type ?? ModifierType.Flat;
-   
+
    /// <summary>Gets the source of the modifier, which is the object that caused the modifier to be applied.</summary>
    public object Source { get; }
-   
+
    /// <summary>Gets the value of the modifier.</summary>
    public float Value { get; }
-   
+
    /// <summary>Initializes a new instance of the <see cref="Modifier"/> struct with the specified value, type, and source.</summary>
    /// <param name="value">The value of the modifier.</param>
    /// <param name="modifierType">The type of the modifier.</param>
@@ -31,9 +31,9 @@ public readonly struct Modifier : IEquatable<Modifier>
    /// <summary>Determines whether the current modifier is equal to another modifier.</summary>
    /// <param name="other">The other modifier to compare with this modifier.</param>
    /// <returns><c>true</c> if the modifiers are equal; otherwise, <c>false</c>.</returns>
-   public bool Equals(Modifier other) => 
-      _type == other._type && Equals(Source, other.Source) && Value.Equals(other.Value);
-   
+   public bool Equals(Modifier other) =>
+      Type == other.Type && Equals(Source, other.Source) && Value.Equals(other.Value);
+
    /// <summary>Determines whether the current modifier object is equal to another modifier object.</summary>
    /// <param name="obj">The modifier object to compare with the current modifier object.</param>
    /// <returns><c>true</c> if the modifier objects are equal; otherwise, <c>false</c>.</returns>
@@ -41,26 +41,24 @@ public readonly struct Modifier : IEquatable<Modifier>
 
    /// <summary>Returns a hash code for the current modifier.</summary>
    /// <returns>A hash code for the current modifier.</returns>
-   public override int GetHashCode() => HashCode.Combine(_type, Source, Value);
+   public override int GetHashCode() => HashCode.Combine(Type, Source, Value);
 
    /// <summary>Determines whether two modifiers are equal.</summary>
-   /// <param name="first">The first modifier to compare.</param>
-   /// <param name="second">The second modifier to compare.</param>
+   /// <param name="left">The first modifier to compare.</param>
+   /// <param name="right">The second modifier to compare.</param>
    /// <returns><c>true</c> if the modifiers are equal; otherwise, <c>false</c>.</returns>
-   public static bool operator ==(Modifier first, Modifier second) => 
-      first.Type == second.Type && 
-      Math.Abs(first.Value - second.Value) < float.Epsilon && 
-      ReferenceEquals(first.Source, second.Source);
-   
+   public static bool operator ==(Modifier left, Modifier right) => left.Equals(right);
+
    /// <summary>Determines whether two modifiers are not equal.</summary>
-   /// <param name="first">The first modifier to compare.</param>
-   /// <param name="second">The second modifier to compare.</param>
+   /// <param name="left">The first modifier to compare.</param>
+   /// <param name="right">The second modifier to compare.</param>
    /// <returns><c>true</c> if the modifiers are not equal; otherwise, <c>false</c>.</returns>
-   public static bool operator !=(Modifier first, Modifier second) => !(first == second);
-   
+   public static bool operator !=(Modifier left, Modifier right) => !left.Equals(right);
+
    /// <summary>Returns a string that represents the current modifier. Logs its value, type and source object.</summary>
    /// <returns>A string that represents the current modifier.</returns>
-   public override string ToString() => $"Value:{Value.ToString(CultureInfo.InvariantCulture)} Type:{Type} Source object: {Source ?? "None"}";
+   public override string ToString() => 
+      $"Value:{Value.ToString(CultureInfo.InvariantCulture)} Type:{Type} Source object: {Source ?? "None"}";
 
    /// <summary>Converts a <see cref="Modifier"/> to a <see cref="float"/> representing its value.</summary>
    /// <param name="modifier">The modifier to convert.</param>
---
+++ Assets/StatSystem/Tests/ModifierTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace StatSystem.Tests
+{
+[TestFixture]
+public class ModifierTests
+{
+   private Modifier _firstModifier;
+   private Modifier _secondModifier;
+   private Modifier _defaultModifier;
+   private Modifier _defaultValuesModifier;
+   
+   [SetUp]
+   public void Setup()
+   {
+      _firstModifier = new Modifier(10, ModifierType.Additive, this);
+      _secondModifier = new Modifier(10, ModifierType.Additive, this);
+      _defaultModifier = default;
+      _defaultValuesModifier = new Modifier(0, ModifierType.Flat);
+   }
+
+   [Test]
+   public void Equals_EqualityForSameModifier_ReturnsTrue() => 
+      Assert.IsTrue(_firstModifier.Equals(_secondModifier));
+
+   [Test]
+   public void Equals_EqualityForDifferentModifiers_ReturnsFalse() => 
+      Assert.IsFalse(_firstModifier.Equals(_defaultValuesModifier));
+
+   [Test]
+   public void Equals_EqualityForDefaultModifer_ReturnsTrue() => 
+      Assert.IsTrue(_defaultModifier.Equals(_defaultValuesModifier));
+
+   [Test]
+   public void OperatorEquals_EqualityForSameModifier_ReturnsTrue() => 
+      Assert.IsTrue(_firstModifier == _secondModifier);
+
+   [Test]
+   public void OperatorEquals_EqualityForDifferentModifiers_ReturnsFalse() => 
+      Assert.IsFalse(_firstModifier == _defaultValuesModifier);
+
+   [Test]
+   public void OperatorEquals_EqualityForDefaultModifer_ReturnsTrue() => 
+      Assert.IsTrue(_defaultModifier == _defaultValuesModifier);
+}
+}
